8f3e606f2a1a391ef919e7249dc1972a4077fc66
kavindyap <86184236+kavindyap@users.noreply.github.com>
2022-03-28 18:58:26 +0530

    Add files via upload

---
+++ 307949_PAMODI_MANANAGE__2017773__Codes_AI_Coursework_4101651_1749966252/AIcw1/DFS.py
@@ -0,0 +1,56 @@
+import random
+
+# Define a function with input as maze
+
+def solve_maze(maze):
+    R, C = len(maze), len(maze[0])  # R, C Rows and Column of the maze
+
+    start = (0, 0)
+    for r in range(R):          # Identification of starting point of the maze
+        for c in range(C):
+            if maze[r][c] == "S":
+                start = (r, c)
+                break
+        else: continue
+        break
+    else:
+        return None
+
+    # Algorithm for DFS
+
+    stack = []
+    stack.append((start[0], start[1], 0))
+    directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    visited = [[False] * C for _ in range(R)]
+
+    while len(stack) != 0:
+        coord = stack.pop()
+        visited[coord[0]][coord[1]] = True
+        print((coord[0], coord[1]))     # print reached coordinates
+        if maze[coord[0]][coord[1]] == "G":
+            return coord[2]
+
+
+        for dir in directions:
+            nr, nc = coord[0] + dir[0], coord[1] + dir[1]
+            if nr < 0 or nr >= R or nc < 0 or nc >= C or maze[nr][nc] == "#" or visited[nr][nc]:
+                continue
+            stack.append((nr, nc, coord[2]+1))
+
+    # Select maze randomly, from saved file
+
+maze_file = ['maze1.txt', 'maze2.txt', 'maze3.txt']
+maze_file_random = random.choice(maze_file)
+
+
+    # open saved text file as a maze
+
+with open(maze_file_random) as file:
+    maze = []
+    for line in file:
+        maze.append([i for i in line.strip("\n")])
+
+    # Print minimum coordinates to gaol & randomly selected maze
+    print("Nodes reach to start point to gaol is (DFS) = ", + solve_maze(maze))
+    print("Randomly selected maze = ", maze_file_random)
+
